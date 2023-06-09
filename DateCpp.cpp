a89f2cf78167bf83a30ee034a918ed0765ad324c
Yuvaraj Palanisamy <yp00801161@techmahindra.com>
2023-06-09 14:22:06 +0200

    Date Programs

---
+++ DateCpp.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+
+void currentTime(){
+    time_t current_dt = time(0);
+    char* result = ctime(&current_dt);
+
+    cout << "The current date and time is:\n" << result << endl;
+}
+
+void year_to_century(int year){
+    cout << "\n  Year to Century \n" ;
+   double cent =   floor((year-1)/100+1);
+   cout << endl << " Year : " << year << " Century : " << cent;
+}
+void leapYear(int year){
+cout << endl << " ~~~~~~~~~~ Leap Year ~~~~~~~~~~~ \n" ;
+bool isLeap = (year%400 ==0) || (year%4==0);
+if (isLeap)
+cout << "/The given Year " << year << " is a Leap year \n" ;
+else
+    cout << "/The given Year " << year << " is not a Leap year \n" ;
+}
+
+int main(){
+currentTime();
+year_to_century(2000);
+year_to_century(1901);
+year_to_century(1800);
+year_to_century(1789);
+year_to_century(2001);
+leapYear(2020);
+leapYear(2019);
+leapYear(2023);
+leapYear(2000);
+}
+
---
+++ DateCpp.h
@@ -0,0 +1,14 @@
+//
+// Created by YP00801161 on 09/06/2023.
+//
+
+#ifndef CPLUSPLUS_DATECPP_H
+#define CPLUSPLUS_DATECPP_H
+
+
+class DateCpp {
+
+};
+
+
+#endif //CPLUSPLUS_DATECPP_H
